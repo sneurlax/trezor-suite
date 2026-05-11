7e5662236dddd54391e6299791b78ed550feb944
Martin Varmuza <mvarmuza@gmail.com>
2026-05-11 23:53:06 +0200

    gnhf 32: Tightened the two `any` annotations on @trezor/transport's `AbstractApiAwaitedResult` callable-shape conditional to `(...args: never[]) => unknown`, eliminating two `any` annotations from the package's public types API.

--- packages/transport-common/src/api/abstract.ts
+++ packages/transport-common/src/api/abstract.ts
@@ -205,7 +205,7 @@ export abstract class AbstractApi extends TypedEmitter<{
 }
 
 export type AbstractApiAwaitedResult<K extends keyof AbstractApi> = AbstractApi[K] extends (
-    ...args: any[]
-) => any
+    ...args: never[]
+) => unknown
     ? Awaited<ReturnType<AbstractApi[K]>>
     : never;
