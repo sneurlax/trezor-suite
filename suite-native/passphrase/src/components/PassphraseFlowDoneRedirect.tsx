75bc2abf686c29677b1bc6e6c68db4538f12230c
juriczech <bohdan.juricek@gmail.com>
2026-05-11 16:33:00 +0200

    fix(suite-native): remove useRedirectOnPassphraseCompletion
    - add exhaustive switch for declarative route rendering to cover all cases or throw
    - remove useRedirectOnPassphraseCompletion -> unnecessary in most cases. The cases who need it should handle this based on the discoveryStatus in a simplified and narrowed manner

--- suite-native/passphrase/src/components/PassphraseFlowDoneRedirect.tsx
+++ suite-native/passphrase/src/components/PassphraseFlowDoneRedirect.tsx
@@ -1,9 +1,67 @@
-import { type ReactNode } from 'react';
+import { type ReactNode, useCallback } from 'react';
+import { useSelector, useStore } from 'react-redux';
 
-import { useRedirectOnPassphraseCompletion } from '../useRedirectOnPassphraseCompletion';
+import { useFocusEffect } from '@react-navigation/native';
 
+import { selectSelectedDevice } from '@suite-common/device';
+import { type DiscoveryRootState, selectDiscoveryByDevicePath } from '@suite-common/wallet-core';
+import { events } from '@suite-native/analytics';
+import { useNavigateToInitialScreen } from '@suite-native/navigation';
+import { useAnalytics } from '@suite-native/services';
+
+import {
+    selectHasPassphraseError,
+    selectPassphraseDiscoveryCompleted,
+} from '../passphraseSelectors';
+
+export const PassphraseFlowFailedRedirect = ({ children }: { children?: ReactNode }) => {
+    const navigateToInitialScreen = useNavigateToInitialScreen();
+
+    useFocusEffect(
+        useCallback(() => {
+            // TODO is this duplication of discovery status switch/case in the stack navigator?
+            // if (hasPassphraseError) {
+            navigateToInitialScreen();
+            // }
+        }, [navigateToInitialScreen]),
+    );
+
+    return children ?? null;
+};
 export const PassphraseFlowDoneRedirect = ({ children }: { children?: ReactNode }) => {
-    useRedirectOnPassphraseCompletion();
+    const passphraseDiscoveryCompleted = useSelector(selectPassphraseDiscoveryCompleted);
+    const device = useSelector(selectSelectedDevice);
+    const analytics = useAnalytics();
+    const store = useStore();
+    const hasPassphraseError = useSelector(selectHasPassphraseError);
+    const navigateToInitialScreen = useNavigateToInitialScreen();
+
+    useFocusEffect(
+        useCallback(() => {
+            // If there is passphrase error, we don't want to go back, but handle errors through alerts within the flow
+            if (passphraseDiscoveryCompleted && !hasPassphraseError) {
+                navigateToInitialScreen();
+
+                const discovery = selectDiscoveryByDevicePath(
+                    store.getState() as DiscoveryRootState,
+                    device?.path,
+                );
+                if (discovery) {
+                    analytics.report({
+                        type: events.passphraseFlowFinishedEvent.name,
+                        payload: { isEmptyWallet: !discovery.hasLoadedAnyNonEmptyAccount },
+                    });
+                }
+            }
+        }, [
+            passphraseDiscoveryCompleted,
+            hasPassphraseError,
+            navigateToInitialScreen,
+            store,
+            device?.path,
+            analytics,
+        ]),
+    );
 
     return children ?? null;
 };
