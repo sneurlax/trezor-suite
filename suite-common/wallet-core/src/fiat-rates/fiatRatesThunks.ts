10dd2237d94c15c14accb6fe8cc61b0459865a4c
Martin Varmuza <mvarmuza@gmail.com>
2026-05-12 09:27:05 +0200

    gnhf 122: Tightened the `Promise<any>` accumulator on the internal `tickerChunks.reduce<Promise<any>>` call in @suite-common/wallet-core's fiatRatesThunks to `Promise<unknown>`, eliminating the file's lone `any` annotation.

--- suite-common/wallet-core/src/fiat-rates/fiatRatesThunks.ts
+++ suite-common/wallet-core/src/fiat-rates/fiatRatesThunks.ts
@@ -340,7 +340,7 @@ export const fetchFiatRatesThunk = createThunk(
                 ),
         );
 
-        tickerChunks.reduce<Promise<any>>(
+        tickerChunks.reduce<Promise<unknown>>(
             (chain, chunk) =>
                 chain.then(() =>
                     dispatch(
