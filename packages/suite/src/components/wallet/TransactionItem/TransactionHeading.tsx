6541f61a50963542470ea5a0b3c2156db7607115
53gur0 <278466100+53gur0@users.noreply.github.com>
2026-06-02 15:26:05 +0200

    fix(suite-desktop): narrow phishing tooltip to zero value check

--- packages/suite/src/components/wallet/TransactionItem/TransactionHeading.tsx
+++ packages/suite/src/components/wallet/TransactionItem/TransactionHeading.tsx
@@ -12,14 +12,14 @@ import { BlurWrapper } from './TransactionItemBlurWrapper';
 type TransactionHeadingProps = {
     transaction: WalletAccountTransaction;
     isPending: boolean;
-    isPhishingTransaction: boolean;
+    isZeroPhishingTransaction: boolean;
     dataTestBase: string;
 };
 
 export const TransactionHeading = ({
     transaction,
     isPending,
-    isPhishingTransaction,
+    isZeroPhishingTransaction,
     dataTestBase,
 }: TransactionHeadingProps) => {
     const symbol = getTxHeaderSymbol(transaction);
@@ -45,10 +45,10 @@ export const TransactionHeading = ({
                 />
             }
             tooltipMaxWidth={250}
-            isActive={isPhishingTransaction}
+            isActive={isZeroPhishingTransaction}
             hasIcon
         >
-            <BlurWrapper $isBlurred={isPhishingTransaction}>
+            <BlurWrapper $isBlurred={isZeroPhishingTransaction}>
                 <Row gap={4} data-testid={`${dataTestBase}/heading`}>
                     <TransactionHeader transaction={transaction} isPending={isPending} />
                     {isSupportedEthStakingNetworkSymbol(transaction.symbol) && (
