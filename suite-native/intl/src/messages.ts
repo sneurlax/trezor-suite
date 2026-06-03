e2633b54e24be254ebba461003e80de21d68ce1b
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-06-03 17:50:10 +0200

    feat(suite-native): swap intent output rendering

--- suite-native/intl/src/messages.ts
+++ suite-native/intl/src/messages.ts
@@ -3551,6 +3551,8 @@ export const messages = {
                 tradedAssetsOutputLabel: 'My assets',
                 tradedAssetsSendLabel: 'You send',
                 tradedAssetsReceiveLabel: 'You receive',
+                swapIntentLabel: 'Intent',
+                swapIntentValue: 'Swap',
                 summary: {
                     label: 'Total including fee',
                     totalAmount: 'Total amount',
