9c00e595c330538e13d6f17877183f83c10d74f2
Mattew Husak <matej.husak@icloud.com>
2026-06-03 23:52:37 +0200

    refactor(suite): move fee modules to @suite-common/fee and @suite/fee (incremental)

--- packages/suite/src/components/wallet/Fees/CollapsibleFees/CollapsibleFees.tsx
+++ packages/suite/src/components/wallet/Fees/CollapsibleFees/CollapsibleFees.tsx
@@ -2,6 +2,12 @@ import { useMemo } from 'react';
 import { useWatch } from 'react-hook-form';
 
 import { Translation } from '@suite/intl';
+import {
+    FeesContext,
+    type FeesContextType,
+    type TronResources,
+    useTransactionMaxFee,
+} from '@suite-common/fee';
 import { type NetworkSymbol, type NetworkType } from '@suite-common/wallet-config';
 import { type FormState } from '@suite-common/wallet-types';
 import { Button, Collapsible, Column, Row, TextButton } from '@trezor/components';
@@ -13,8 +19,6 @@ import {
 import { CustomFee } from './CustomFee/CustomFee';
 import { CustomFeeTron } from './CustomFee/CustomFeeTron';
 import { StandardFee } from './StandardFee/StandardFee';
-import { FeesContext, type FeesContextType, type TronResources } from '../context/FeesContext';
-import { useTransactionMaxFee } from './hooks/useTransactionMaxFee';
 
 export type CollapsibleFeesProps = {
     networkSymbol: NetworkSymbol;
